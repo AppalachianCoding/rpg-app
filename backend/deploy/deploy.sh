edcb2d2a6209f79fa9b8bae15bfd8863c3b670c2
Reid Bandy <reidbandy@gmail.com>
2025-02-13 13:18:39 -0500

    Fix aws region assignment again

--- backend/deploy/deploy.sh
+++ backend/deploy/deploy.sh
@@ -5,7 +5,6 @@ ACCT_ID="$(aws sts get-caller-identity --query Account --output text)"
 if [[ -z "$AWS_REGION" ]]; then
   AWS_REGION="$(aws configure get region)"
 fi
-AWS_REGION="$(aws configure get region)"
 CF_BUCKET="$ACCT_ID-$AWS_REGION-cfbucket"
 STACK_NAME="$1"
 DOCKER_IMAGE="$STACK_NAME:latest"
