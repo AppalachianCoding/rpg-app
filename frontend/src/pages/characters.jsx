6de1394158841ce8718935f5d000f4709e260806
Yetalon <Davisac7@etsu.edu>
2025-02-17 14:40:51 -0500

    updated navbar created characters landing page

---
+++ frontend/src/pages/characters.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+import CharacterCard from "../componet/charactercard";
+
+function characters() {
+    const [character, setCharacters] = useState([]);
+
+    return(
+        <div>
+
+        </div>
+    );
+}
+
+export default characters;
